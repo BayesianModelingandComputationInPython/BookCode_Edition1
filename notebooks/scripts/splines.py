5477242b370fa4eb74814cc060c862890a76fbd5
aloctavodia <aloctavodia@gmail.com>
2021-12-20 10:38:37 +0200

    clean code, add headers, updated script and data

--- notebooks/scripts/splines.py
+++ notebooks/scripts/splines.py
@@ -1,84 +1,50 @@
-# Convenience function to compute the β cofficients
+import numpy as np
+import matplotlib.pyplot as plt
+
+def splines(knots, x_true=None, y_true=None):
+    # Get x-y values from true function
+    if x_true is None:
+        x_min, x_max = 0, 6
+        x_true = np.linspace(x_min, x_max, 200)
+    else:
+        x_min, x_max = min(x_true), max(x_true)  
+    
+    if y_true is None:
+        y_true = np.sin(x_true)
+
+    # Prepare figure
+    _, axes = plt.subplots(2, 2, figsize=(9, 6),
+                         constrained_layout=True,
+                         sharex=True, sharey=True)
+    axes = np.ravel(axes)
+
+    # Plot the knots and true function
+    for ax in axes:
+        ax.vlines(knots, -1, 1, color='grey', ls='--')
+        ax.plot(x_true, y_true, 'C4.', lw=4, alpha=0.5)
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    labels = ["Piecewise Constant", "Piecewise Linear", "Piecewise Quadratic", "Piecewise Cubic"]
+    for order, ax, label in zip(range(0, 4), axes, labels):
+        B = basis(x_true, order, knots)
+        y_hat = ols(B, y_true)
+        ax.plot(x_true, y_hat, c='k')
+        ax.set_title(label)
+
+def basis(x_true, order, knots):
+    """compute basis functions"""
+    B = []
+    for i in range(0, order+1):
+        B.append(x_true**i)
+    for k in knots:
+        B.append(np.where(x_true < k, 0, (x_true - k) ** order))
+    B = np.array(B).T
+    return B
+
+
 def ols(X, y):
     """Compute ordinary least squares in closed-form"""
     β = np.linalg.solve(X.T @ X, X.T @ y)
     return X @ β
-
-
-# Define the knots
-knots = 1.57, 4.71
-
-# Get x-y values from true function
-x_min, x_max = 0, 6
-x_true = np.linspace(x_min, x_max, 200)
-y_true = np.sin(x_true)
-
-# Split the x-y values into 3 intervals defined by the knots
-x_intervals = [x_true[x_true <= knots[0]],
-               x_true[(knots[0] < x_true) & (x_true < knots[1])],
-               x_true[x_true >= knots[1]],
-               ]
-
-y_intervals = [y_true[x_true <= knots[0]],
-               y_true[(knots[0] < x_true) & (x_true < knots[1])],
-               y_true[x_true >= knots[1]],
-               ]
-
-# Prepare figure
-_, ax = plt.subplots(2, 2, figsize=(9, 6), 
-                     constrained_layout=True, sharex=True, sharey=True)
-ax = np.ravel(ax)
-
-# Plot the knots and true function
-for ax_ in ax:
-    ax_.vlines(knots, -1, 1, color='grey', ls='--')
-    ax_.plot(x_true, y_true, 'C4', lw=4, alpha=0.5)
-    ax_.set_xticks([])
-    ax_.set_yticks([])
-
-# Compute and plot pointwise step function
-ax[0].hlines(y_intervals[0].mean(), color='k', xmin=x_min, xmax=knots[0])
-ax[0].hlines(y_intervals[1].mean(), color='k', xmin=knots[0], xmax=knots[1])
-ax[0].hlines(y_intervals[2].mean(), color='k', xmin=knots[1], xmax=x_max)
-ax[0].set_title('Piecewise Constant')
-
-# Compute and plot pointwise linear function
-B = np.array(
-    [np.ones_like(x_true),
-     x_true,
-     np.where(x_true < knots[0], 0, (x_true - knots[0])),
-     np.where(x_true < knots[1], 0, (x_true - knots[1])),
-     ]).T
-
-y_hat = ols(B, y_true)
-ax[1].plot(x_true, y_hat, c='k')
-ax[1].set_title('Piecewise Linear')
-
-# Compute and plot pointwise quadratic function
-B = np.array(
-    [np.ones_like(x_true),
-     x_true,
-     x_true ** 2,
-     np.where(x_true < knots[0], 0, (x_true - knots[0]) ** 2),
-     np.where(x_true < knots[1], 0, (x_true - knots[1]) ** 2),
-     ]).T
-
-y_hat = ols(B, y_true)
-ax[2].set_title('Piecewise Quadratic')
-ax[2].plot(x_true, y_hat, color='k')
-
-# Compute and plot pointwise cubic function
-B = np.array(
-    [np.ones_like(x_true),
-     x_true,
-     x_true ** 2,
-     x_true ** 3,
-     np.where(x_true < knots[0], 0, (x_true - knots[0]) ** 3),
-     np.where(x_true < knots[1], 0, (x_true - knots[1]) ** 3),
-     ]).T
-
-y_hat = ols(B, y_true)
-ax[3].set_title('Piecewise Cubic')
-ax[3].plot(x_true, y_hat, color='k')
-plt.savefig("img/chp05/piecewise.png")
 
